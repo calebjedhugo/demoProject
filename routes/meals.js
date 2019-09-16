6c024e4dcb09437863bf2bb0f7ddefa4e60b2aa7
Caleb Hugo <calebjedhugo@gmail.com>
2019-09-16 13:17:42 -0400

    Put role authentication in the 'all' to reduce redundant code.

--- routes/meals.js
+++ routes/meals.js
@@ -4,12 +4,17 @@ const User = require('../model/User'); //for validating the user's role.
 
 const {mealValidation, getMealsValidation, skipMaxSearchValidation} = require('../validation')
 
-router.route('/').get(async (req, res) => {
-  const requesterRole = (await User.findOne({_id: req.user._id})).role
-  if(requesterRole !== 'admin' && req.user._id !== req.query._id){
-    return res.status(401).json('Insufficient Role') //Only admins should be viewing meals that aren't theirs.
-  }
-
+router.route('/').all(async (req, res, next) => {
+  if(req.query._id){
+    if(req.query._id !== req.user._id){
+      let requesterRole = (await User.findOne({_id: req.user._id})).role
+      if(requesterRole !== 'admin'){
+        return res.status(401).json('Insufficient Role') //Only admins should doing anything to someone elses meals.
+      }
+    }
+  } else req.query._id = req.user._id //Of course you can edit yourself.
+  next()
+}).get(async (req, res) => {
   const {error} = getMealsValidation(req.query)
   if(error) return res.status(400).json(error.details[0].message);
 
@@ -25,11 +30,6 @@ router.route('/').get(async (req, res) => {
   }
   res.json(mealData);
 }).post(async (req, res) => {
-  const requesterRole = (await User.findOne({_id: req.user._id})).role
-  if(requesterRole !== 'admin' && req.user._id !== req.body.userId){
-    res.status(401).json('Insufficient Role') //Only admins should be able to add meals to other accounts.
-  }
-
   const { error } = mealValidation(req.body);
   if(error) return res.status(400).json(error.details[0].message);
 
@@ -43,12 +43,7 @@ router.route('/').get(async (req, res) => {
   }
 }).patch(async (req, res) => {
   const { error } = mealValidation(req.body);
-  const requesterRole = (await User.findOne({_id: req.user._id})).role
   if(error) return res.status(400).json(error.details[0].message);
-
-  if(requesterRole !== 'admin' && req.user._id !== req.body.userId){
-    return res.status(401).json('Insufficient Role') //Only admins can update a single record that's not theirs.
-  }
 
   try {
     const updatedMeal = await Meal.findOneAndUpdate({_id: req.body._id}, req.body, {new: true}).select({userId: 0})
@@ -58,11 +53,6 @@ router.route('/').get(async (req, res) => {
   }
 }).delete(async (req,res) => {
   var query = {_id: req.body._id}
-  const requesterRole = (await User.findOne({_id: req.user._id})).role
-  //Only admins can delete a single record that's not theirs.
-  if(requesterRole !== 'admin' && req.user._id !== req.body.userId){
-    res.status(401).json('Insufficient Role')
-  }
 
   try {
     await Meal.deleteOne(query);
